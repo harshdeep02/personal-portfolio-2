c88659070d1d2a6ed028891bd2a53395a57771a4
Harsh Deep <harshdeep7887@gmail.com>
2023-11-27 10:56:46 +0530

    first commit

--- portfolio.js
+++ portfolio.js
@@ -32,7 +32,6 @@ window.onscroll = function(){
 
 let menuBar =document.getElementById("menuBar")
 let navId = document.getElementById("navId")
-
 // function toggleHide (){
 //     if(navId.style.visibility){
 //         navId.style.visibility = "hidden"
@@ -46,8 +45,49 @@ let navId = document.getElementById("navId")
 menuBar.addEventListener('click', function run(){
     if(navId.style.display != 'block'){
         navId.style.display ='block'
-    }
+          }
     else{
         navId.style.display='none'
     }
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
